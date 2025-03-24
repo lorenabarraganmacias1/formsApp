51caed1e3c541bf43e2bf2f132adf91d86924a02
Lorena Barragan <lorenabarraganmacias1@gmail.com>
2025-03-24 18:52:07 -0500

    inicios de logica del formulario

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``
 })
 export class BasicPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name:[''],
+    price:[0],
+    inStorage:[0],
+
+
+  })
+
+  constructor(private fb: FormBuilder){}
+
+  onSave():void{
+    console.log(this.myForm.value)
+  }
+
+  
 
 }
